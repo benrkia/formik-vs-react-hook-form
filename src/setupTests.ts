b35ff22c1183a46b464f91d73bc73eca2997fe03
benrkia ilyasse <benrkyailyass@gmail.com>
2020-08-19 14:12:05 +0100

    Get the boilerplate ready

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,7 +2,6 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/react/cleanup-after-each';
 import '@testing-library/jest-dom/extend-expect';
 
 const localStorageMock: Storage = { // This will be provided for all other test files
